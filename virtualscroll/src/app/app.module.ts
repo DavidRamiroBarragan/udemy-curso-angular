98307c2cec663b958496196b47e2781e60443688
David <dramirobarragan@gmail.com>
2019-03-26 19:42:31 +0100

    Starting section 21

---
+++ virtualscroll/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { VirtualComponent } from './virtual/virtual.component';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VirtualComponent
+  ],
+  imports: [
+    BrowserModule,
+    ScrollingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
